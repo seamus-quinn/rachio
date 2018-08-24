0b54353059c1c63bc85decb4613ebe54ee45db56
Seamus Quinn <sumaesquinn@gmail.com>
2018-08-23 19:02:01 -0600

    Refactor to have rows in devices

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
+const apiKey = "76980330-8f0b-4659-a341-527364acf134"
 
 function getDevices() {
-  const apiKey = "76980330-8f0b-4659-a341-527364acf134"
   const url = "https://api.rach.io/1/public/person/2ee8a9ca-741d-4b1a-add3-8a7683e5aa28"
   fetch(url, {
     method: "GET",
@@ -19,56 +19,33 @@ function displayDevices(devices) {
       `<div class="device">
         <h1 class="device-name">${device.name}</h1>
         <p class="device-status">${device.status}</p>
-        <div class="zone-container">
-          <div class="zones">
-            <h1 class="title">Zones</h1>
-            <div class="zone-list">
-              ${populateZoneList(device.zones)}
-            </div>
-          </div>
-          <div class="time">
-            <h1 class="title">Duration (s)</h1>
-            <div class="time-list">
-              ${populateTimeList(device.zones)}
-            </div>
-          </div>
-          <div class="selects">
-            <h1 class="title">Run</h1>
-            <div class="select-list">
-              ${populateSelectList(device.zones)}
-            </div>
-          </div>
-          <div class="run-button-container">
-            <button class="run-selected-button">Run Selected Zones
-            </button>
-          </div>
+        <div class="table">
+          <h1 class="title">Zones</h1>
+          <h1 class="title">Duration</h1>
+          <h1 class="title">Run</h1>
+          ${createRows(device.zones)}
+        </div>
+        <div class="submit-button-container">
+          <button class="submit-button">Submit</button>
         </div>
       </div>`
     )
   })
 }
 
-function populateZoneList(zones) {
-  let zoneElements = zones.map(zone => {
-    return `<p class="zone">${zone.name}</p>`
+function createRows(zones) {
+  let rows = zones.map(zone => {
+    return `<div class="row" id="${zone.id}">
+      <p class="zone">${zone.name}</p>
+      <input type="text" value="${zone.runtime}"/>
+      <input type="checkbox" checked/>
+     </div>
+    `
   })
-  return zoneElements.join('')
+  return rows.join('')
 }
 
-function populateTimeList(zones) {
-  let timeElements = zones.map(zone => {
-    return `<input type="text" value="${zone.runtime}"/>`
-  })
-  return timeElements.join('')
-}
 
-function populateSelectList(zones) {
-  let selects = []
-  for(var i = 0; i < zones.length; i++) {
-    selects.push(`<input type="checkbox" checked/>`)
-  }
-  return selects.join('')
-}
 
 getDevices();
 
