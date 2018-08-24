c2ece3806101532b9b68d43fb96101477f3732ca
Seamus Quinn <sumaesquinn@gmail.com>
2018-08-23 20:48:00 -0600

    add toggleChecked function and select nodes for put method

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const apiKey = "76980330-8f0b-4659-a341-527364acf134"
 
+$('.device-container').on('click', '.submit-button', createStartObject)
+$('.device-container').on('click', '.checkbox', toggleChecked)
+
 function getDevices() {
   const url = "https://api.rach.io/1/public/person/2ee8a9ca-741d-4b1a-add3-8a7683e5aa28"
   fetch(url, {
@@ -16,7 +19,7 @@ function getDevices() {
 function displayDevices(devices) {
   devices.forEach(device => {
     $('.device-container').append(
-      `<div class="device">
+      `<div class="device" id="${device.id}">
         <h1 class="device-name">${device.name}</h1>
         <p class="device-status">${device.status}</p>
         <div class="table">
@@ -26,7 +29,7 @@ function displayDevices(devices) {
           ${createRows(device.zones)}
         </div>
         <div class="submit-button-container">
-          <button class="submit-button">Submit</button>
+          <button class="submit-button" id="${device.id}">Submit</button>
         </div>
       </div>`
     )
@@ -34,15 +37,32 @@ function displayDevices(devices) {
 }
 
 function createRows(zones) {
-  let rows = zones.map(zone => {
-    return `<div class="row" id="${zone.id}">
+  let rows = zones.map((zone, index) => {
+    return `<div 
+        class="row"
+        data-duration="${zone.runtime}"
+        data-id=${zone.id}"
+        data-sortOrder="${index + 1}"
+        data-checked=true
+        >
       <p class="zone">${zone.name}</p>
       <input type="text" value="${zone.runtime}"/>
-      <input type="checkbox" checked/>
+      <input class="checkbox" type="checkbox" checked/>
      </div>
     `
   })
   return rows.join('')
+}
+
+function toggleChecked() {
+  let val = $(this).prop('checked')
+  $(this).parent().attr('data-checked', val)
+}
+
+function createStartObject() {
+  let foo = Array.from($(this).parent().siblings('.table').children('.row')).map(node => node.dataset)
+
+  console.log(foo)
 }
 
 
